b18e50e9ce0f949db6590da3164cbda248a26528
@kolayv <kolayv@pm.me>
2019-07-07 23:51:09 +0300

    upd
    Signed-off-by: @kolayv <kolayv@pm.me>

--- functions/vnc.fish
+++ functions/vnc.fish
@@ -1,5 +1,5 @@
 function vnc
   vnc4server -kill :1
   sleep 1
-  vnc4server :1 -geometry 1280x720 -depth 24
+  vnc4server :1 -depth 24 -geometry 1280x720 # 1920x1080
 end
---
+++ functions/vue.fish
@@ -0,0 +1,4 @@
+# nvm serve wrapper
+function vue
+	__nvm_run "vue" $argv
+end
